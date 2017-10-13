26cc284d15b94c0684b226654638d92487127ed1
Aldwin Hermanudin <aldwinakbar@gmail.com>
2017-10-12 19:28:08 -0700

    added bubble sort information function

---
+++ bubble-sort-info.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void bubble_sort(vector<int> &a, int &swaps){
+    int n = a.size();
+    swaps = 0;
+    for (int i = 0; i < n; i++) {
+        // Track number of elements swapped during a single array traversal
+        int numberOfSwaps = 0;
+
+        for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                numberOfSwaps++;
+            }
+        }
+
+        // If no elements were swapped during a traversal, array is sorted
+        if (numberOfSwaps == 0) {
+            break;
+        }
+        swaps += numberOfSwaps;
+    }
+}
+
+int main() {
+   int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    int swaps;
+    bubble_sort(a,swaps);
+    cout << "Array is sorted in "<< swaps <<" swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[n-1] << endl;
+  return 0;
+}
